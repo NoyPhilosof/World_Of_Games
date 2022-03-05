c36525aaba95a0147b063c68373b9e9733de1407
NoyPhilosof <96237612+NoyPhilosof@users.noreply.github.com>
2022-03-05 12:50:03 +0200

    Update MainScores.py

--- flask/MainScores.py
+++ flask/MainScores.py
@@ -45,5 +45,5 @@ def failure():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', debug=True, port=5000)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5000)
+    # app.run(host='0.0.0.0', debug=True)
